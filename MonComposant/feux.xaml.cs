1c58f4f803f5822a0e0f18eb8fc0b5573e5a8c74
thaouet <thaouet@outlook.com>
2018-04-27 12:21:12 +0100

    Ajoutez des fichiers projet.

---
+++ MonComposant/feux.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MonComposant
+{
+
+    public delegate void FeuHandler(Object sender, EventArgs e);
+    
+    /// <summary>
+    /// Logique d'interaction pour feux.xaml
+    /// </summary>
+    public partial class feux : UserControl
+    {
+
+        public event FeuHandler CouleurChangeEvent;
+
+        public String couleur;
+        public feux()
+        {
+            InitializeComponent();
+        }
+
+        private void bRed_Click(object sender, RoutedEventArgs e)
+        {
+            bRed.Background = Brushes.Red;
+            bGreen.Background = Brushes.LightGray;
+            bOrange.Background = Brushes.LightGray;
+            couleur = "Rouge";
+            CouleurChangeEvent(this, new EventArgs());
+        }
+
+        private void bOrange_Click(object sender, RoutedEventArgs e)
+        {
+            bRed.Background = Brushes.LightGray;
+            bGreen.Background = Brushes.LightGray;
+            bOrange.Background = Brushes.Orange;
+            couleur = "Orange";
+            CouleurChangeEvent(this, new EventArgs());
+        }
+
+        private void bGreen_Click(object sender, RoutedEventArgs e)
+        {
+            bRed.Background = Brushes.LightGray;
+            bGreen.Background = Brushes.Green;
+            bOrange.Background = Brushes.LightGray;
+            couleur = "Vert";
+            CouleurChangeEvent(this, new EventArgs());
+        }
+    }
+}
---
+++ composantDemo/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xaml;
+
+namespace composantDemo
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            feux1.CouleurChangeEvent += new MonComposant.FeuHandler(Feux1_CouleurChangeEvent);
+        }
+
+        private void Feux1_CouleurChangeEvent(object sender, EventArgs e)
+        {
+            labelfeux.Content = feux1.couleur;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
